babca5788f6dc19e57586b7f7d2c561c19900766
wushaohang <781920860@qq.com>
2025-05-01 22:35:44 +0800

    add("自动回复")

--- learn_project/模拟淘宝客服自动回复.py
+++ learn_project/模拟淘宝客服自动回复.py
@@ -1,5 +1,24 @@
-def find_answer():
-    with open('replay.txt','r',encoding = 'gbk') as file:
+def find_answer(question):
+
+    with open('replay.txt','r',encoding = 'utf-8') as file:
         while True:
-            line = file.writeline()
+            line = file.readline()
             if line == '':
+                break
+            keyword = line.split('|')[0]
+            reply =  line.split('|')[1]
+            if keyword in question:
+                return  reply
+        return False
+if __name__ == '__main__':
+    question = input('请讲：')
+    while True:
+        if question == 'bye':
+            break
+        else:
+            replay = find_answer(question)
+            if replay==False:
+                question = input('不知道你说的什么')
+            else:
+                print(replay)
+                question = input('再问问？')
